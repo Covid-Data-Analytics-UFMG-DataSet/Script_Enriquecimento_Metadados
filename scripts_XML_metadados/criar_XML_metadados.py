b5369df95f637555ded957c54151d71ab8d0fc7a
Gabrielnmds <69644988+Gabrielnmds@users.noreply.github.com>
2020-09-21 22:53:50 -0300

    script p/gerar XML a partir da tabela de metadados

---
+++ scripts_XML_metadados/criar_XML_metadados.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree as xml
+import pandas as pd
+
+def generate_xml(tabela_excel):
+
+    df = pd.ExcelFile(tabela_excel)
+    list_sheets = df.sheet_names
+    meta = pd.read_excel(df, list_sheets[0])
+    header = pd.read_excel(df, list_sheets[1])
+
+    root = xml.Element('Arquivos')
+    i = 0
+    for row in meta.iterrows():
+        i += 1
+        arquivo = xml.SubElement(root, "ID_Arquivo", name=str(i))
+
+        for col in header.iterrows():
+            nome_XML = col[1].loc['Nome XML']
+            nome_original = str(col[1].loc['Nome original'])
+            xml.SubElement(arquivo, nome_XML, name=nome_original).text = str(row[1].loc[nome_original])
+
+
+    tree = xml.ElementTree(root)
+
+    with open("metadados.xml", "wb") as file:
+        tree.write(file)
